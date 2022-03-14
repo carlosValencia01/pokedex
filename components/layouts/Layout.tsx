53c4801e72b74ec2e0bddc756abda8374c0623a2
Carlos Valencia <jocavalenciava@ittepic.edu.mx>
2022-03-14 12:20:33 -0700

    Feat: Pokedex app show main page and specific pokemon page working

---
+++ components/layouts/Layout.tsx
@@ -0,0 +1,28 @@
+import { FC } from "react"
+import Head from "next/head"
+import { Navbar } from "../ui"
+
+interface Props {
+  title?:string;
+}
+
+export const Layout:FC<Props> = ({children, title}) => {
+  return (
+    <>
+        <Head>
+            <title>{title || 'Pokemon App'}</title>
+            <meta name="author" content="Carlos Valencia"/>
+            <meta name="description" content={`Informacion sobre el pokemon ${title}`}/>
+            <meta name="keywords" content={`${title}, pokemon, pokedex`}/>
+        </Head>
+
+        <Navbar/>
+
+        <main style={{
+          padding:'0px 20px'
+        }}>
+            {children}
+        </main>
+    </>
+  )
+}
---
+++ pages/favorites/index.tsx
@@ -0,0 +1,10 @@
+import { Layout } from '../../components/layouts'
+
+export const index = () => {
+  return (
+    <Layout title='Pokemons - Favoritos'><h1>hola favoritos</h1></Layout>
+  )
+}
+
+
+export default index;
